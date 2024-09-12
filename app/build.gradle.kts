59734c78ba19e7d40c6909ff3871dd9b4d4d0b19
PENAN RAJPUT <penanrajput1998@gmail.com>
2024-09-13 00:50:26 +0530

    full code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+   
 }
 
 android {
@@ -29,6 +31,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -37,6 +47,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,19 +5,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
@@ -53,8 +60,37 @@ public class MainActivity extends AppCompatActivity {
 
 
         NetworkTask networkTask = new NetworkTask();
-        networkTask.execute("https://www.google.com", "https://www.github.com");
+        EditText editTextText = findViewById(R.id.editTextText);
+        String query = editTextText.getText().toString();
+        networkTask.execute("https://api.github.com/search/users?q=" + query);
     }
+
+    ArrayList<GithubUser> parseJson(String s) throws JSONException {
+        ArrayList<GithubUser> githubUsers = new ArrayList<>();
+
+        // parse the json
+        try {
+            JSONObject root = new JSONObject(s);
+            JSONArray items = root.getJSONArray("items");
+
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject object = items.getJSONObject(i);
+                String login = object.getString("login");
+                Integer id = object.getInt("id");
+                String avatar_url = object.getString("avatar_url");
+                Double score = object.getDouble("score");
+                String html = object.getString("html_url");
+                String avatar = object.getString("avatar_url");
+                GithubUser githubUser = new GithubUser(login, id, html, score, avatar);
+                githubUsers.add(githubUser);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return githubUsers;
+    }
+
 
     // 1st parameter is String because we will provide URL which is in String format
     // 2nd parameter is Void because we don't need/show progress update via API Call
@@ -92,8 +128,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            TextView textView = findViewById(R.id.textView);
-            textView.setText(s);
+            ArrayList<GithubUser> users = null;
+            try {
+                users = parseJson(s);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+//            Log.e("MainActivity", "onPostExecute: " + users.size());
+//            Log.e("MainActivity", "onPostExecute: " + users.get(7).getHtml_url());
+            GithubUserAdapter githubUserAdapter = new GithubUserAdapter(users);
+            RecyclerView recyclerView = findViewById(R.id.rvUsers);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+            recyclerView.setAdapter(githubUserAdapter);
+
+//            TextView textView = findViewById(R.id.textView);
+//            textView.setText(s);
         }
     }
 }
+
